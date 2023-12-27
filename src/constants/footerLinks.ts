f357c5f1d773b6ff436e8e09e3e7e8074eb08846
Yan Yermalayeu <yermik2014@yandex.ru>
2023-12-28 00:18:53 +0300

    feat: add footer links and layout

---
+++ src/constants/footerLinks.ts
@@ -0,0 +1,9 @@
+import { pathnames } from "@/config"
+
+export const footerLinks = [
+  { href: pathnames["/"], localeName: "home" },
+  { href: pathnames["/posts"], localeName: "blog" },
+  { href: pathnames["/about"], localeName: "about" },
+  { href: pathnames["/contact"], localeName: "contact" },
+  { href: pathnames["/privacy"], localeName: "privacy" },
+]
---
+++ src/constants/footerSocials.ts
@@ -0,0 +1,27 @@
+import facebook from "@/assets/icons/facebook.svg"
+import instagram from "@/assets/icons/instagram.svg"
+import linkedin from "@/assets/icons/linkedin.svg"
+import twitter from "@/assets/icons/twitter.svg"
+
+export const footerSocials = [
+  {
+    image: facebook,
+    name: "facebook",
+    href: "https://www.facebook.com/",
+  },
+  {
+    image: instagram,
+    name: "instagram",
+    href: "https://www.instagram.com/x.yermik/",
+  },
+  {
+    image: linkedin,
+    name: "linkedin",
+    href: "https://www.linkedin.com/in/john-yermalayeu-a9aab0250/",
+  },
+  {
+    image: twitter,
+    name: "twitter",
+    href: "https://twitter-clone-xyermikx.vercel.app/",
+  },
+]
