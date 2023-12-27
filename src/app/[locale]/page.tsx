1f3c9334be15b88e46510b3a2945613e1056ab47
Yan Yermalayeu <yermik2014@yandex.ru>
2023-12-27 22:36:37 +0300

    fix: static rendering with vercel

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,10 @@
 import { useTranslations } from "next-intl"
+import { unstable_setRequestLocale } from "next-intl/server"
 
 import styles from "./page.module.scss"
 
-export default function Home() {
+export default function Home({ params: { locale } }: { params: { locale: string } }) {
+  unstable_setRequestLocale(locale)
   const t = useTranslations("home")
   return <main className={styles.main}>{t("title")}</main>
 }
