85d4a950fe285dc3145010f5324e16825b21905c
Yan Yermalayeu <yermik2014@yandex.ru>
2023-12-29 02:10:37 +0300

    feat: add authors section

---
+++ src/components/AuthorCard/AuthorCard.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import { footerSocials } from "@/constants/footerSocials"
+
+import styles from "./authorCard.module.scss"
+
+interface IAuthorCardProps {
+  author: {
+    logo: string
+    name: string
+    company: string
+    role: string
+  }
+}
+
+export function AuthorCard({ author: { logo, name, company, role } }: IAuthorCardProps) {
+  return (
+    <div className={styles.card}>
+      <Image src={logo} alt={name} />
+      <h4>{name}</h4>
+      <p>
+        {role} @{company}
+      </p>
+      <ul className={styles.links}>
+        {footerSocials.map(({ href, image, name: linkName }) => (
+          <li key={name}>
+            <Link href={href} target="_blank">
+              <Image src={image} alt={linkName} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
---
+++ src/constants/logos.ts
@@ -0,0 +1,13 @@
+import logo1 from "@/assets/icons/logo1.svg"
+import logo2 from "@/assets/icons/logo2.svg"
+import logo3 from "@/assets/icons/logo3.svg"
+import logo4 from "@/assets/icons/logo4.svg"
+import logo5 from "@/assets/icons/logo5.svg"
+
+export const featuredLogos = [
+  { logo: logo1, alt: "logo1" },
+  { logo: logo2, alt: "logo2" },
+  { logo: logo3, alt: "logo3" },
+  { logo: logo4, alt: "logo4" },
+  { logo: logo5, alt: "logo5" },
+]
