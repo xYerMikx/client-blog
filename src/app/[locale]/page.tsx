aea46850cfc0f851656b1debfe22392be8c86945
Yan Yermalayeu <yermik2014@yandex.ru>
2023-12-28 15:38:56 +0300

    feat: add hero section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,15 @@
-import { useTranslations } from "next-intl"
 import { unstable_setRequestLocale } from "next-intl/server"
+
+import { Hero } from "@/components/Hero/Hero"
 
 import styles from "./page.module.scss"
 
 export default function Home({ params: { locale } }: { params: { locale: string } }) {
   unstable_setRequestLocale(locale)
-  const t = useTranslations("home")
-  return <main className={styles.main}>{t("title")}</main>
+
+  return (
+    <main className={styles.main}>
+      <Hero />
+    </main>
+  )
 }
