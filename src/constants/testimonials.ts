96cf873b194164c6419af596d6d52b52096ee507
Yan Yermalayeu <yermik2014@yandex.ru>
2023-12-30 23:51:25 +0300

    feat: add testimonials section

---
+++ src/constants/testimonials.ts
@@ -0,0 +1,27 @@
+import dianne from "@/assets/icons/dianne.svg"
+import floyd from "@/assets/icons/floyd.svg"
+import jonathan from "@/assets/icons/jonathan.svg"
+
+export const testimonials = [
+  {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    photo: jonathan,
+    authorName: "Jonathan Vallem",
+    location: "New york, USA",
+    id: 1,
+  },
+  {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    photo: floyd,
+    authorName: "Floyd Miles",
+    location: "Warsaw, Poland",
+    id: 2,
+  },
+  {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    photo: dianne,
+    authorName: "Dianne Russell",
+    location: "Miami, USA",
+    id: 3,
+  },
+]
