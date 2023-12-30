68dd9627e6600a777e3a83d87fd9a261dcab4d97
Yan Yermalayeu <yermik2014@yandex.ru>
2023-12-31 00:26:02 +0300

    feat: add modal feature

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { useState } from "react"
+import { createPortal } from "react-dom"
+
+import { Button } from "@/ui/Button/Button"
+
+import styles from "./modal.module.scss"
+
+interface IModalProps {
+  btnText: string
+}
+
+export function Modal({ btnText }: IModalProps) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function openModal() {
+    setIsOpen(true)
+    document.body.style.overflow = "hidden"
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+    document.body.style.overflow = "auto"
+  }
+
+  return (
+    <>
+      <Button onClick={openModal} variant="secondary">
+        {btnText}
+      </Button>
+      {isOpen &&
+        createPortal(
+          <div className={styles.overlay}>
+            <div className={styles.content}>
+              <button onClick={closeModal} className={styles.close} />
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </div>
+          </div>,
+          document.body,
+        )}
+    </>
+  )
+}
