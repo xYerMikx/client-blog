d8139c55c7d27c2516fe4bbd7602fb9dd0e9fd46
Yan Yermalayeu <yermik2014@yandex.ru>
2023-12-31 19:52:20 +0300

    feat: add upper section layout

---
+++ src/app/[locale]/contact/page.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import Link from "next/link"
+
+import styles from "./contact.module.scss"
+
+export default function Contact() {
+  return (
+    <main className={styles.contact}>
+      <div>
+        <h6 className={styles.contact__suptitle}>Contact us</h6>
+        <h1 className={styles.contact__title}>Let’s Start a Conversation</h1>
+        <p className={styles.contact__text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
+        </p>
+        <div className={styles.contact__info}>
+          <div>
+            <h6 className={styles.info__title}>Working hours</h6>
+            <div className={styles.info__worktime}>
+              <span>Monday To Friday</span>
+              <span>9:00 AM to 8:00 PM </span>
+              <p className={styles.info__available}>Our Support Team is available 24/7</p>
+            </div>
+          </div>
+          <div>
+            <h6 className={styles.info__title}>Contact Us</h6>
+            <div className={styles.info__links}>
+              <Link className={styles.info__tel} href="tel:+375291112233">
+                020 7993 2905
+              </Link>
+              <Link className={styles.info__email} href="mailto:hello@finsweet.com">
+                hello@finsweet.com
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
