94c724e0a2cc3a85ed3677ceec19613d38e32b38
Yan <101996018+xYerMikx@users.noreply.github.com>
2024-01-02 00:24:20 +0300

    feat: add authors section (#10)
    * feat: add authors section
    
    * fix:fix authors
    
    * fix: authors amount and key bug

---
+++ src/components/Authors/Authors.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import { authors } from "@/constants/authors"
+import { featuredLogos } from "@/constants/logos"
+
+import { AuthorCard } from "../AuthorCard/AuthorCard"
+import styles from "./authors.module.scss"
+
+export function Authors() {
+  return (
+    <section className={styles.authors}>
+      <h2>List of Authors</h2>
+      <div className={styles.authors__list}>
+        {authors.slice(0, 4).map((author) => (
+          <AuthorCard key={author.id} {...author} />
+        ))}
+      </div>
+      <div className={styles.logos}>
+        <p className={styles.spans}>
+          <span>We are</span>
+          <span>Featured in</span>
+        </p>
+        {featuredLogos.map(({ logo, alt }) => (
+          <Link key={alt} href="/">
+            <Image src={logo} alt={alt} />
+          </Link>
+        ))}
+      </div>
+    </section>
+  )
+}
