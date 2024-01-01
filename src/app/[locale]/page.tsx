9aed2aa88cdb8f16c689ca3623162aec62454318
Yan Yermalayeu <yermik2014@yandex.ru>
2024-01-02 00:47:01 +0300

    fix: sections margins

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,13 @@
 import { unstable_setRequestLocale } from "next-intl/server"
 
+import { About } from "@/components/About/About"
+import { Authors } from "@/components/Authors/Authors"
+import { ChooseCategory } from "@/components/ChooseCategory/ChooseCategory"
 import { Hero } from "@/components/Hero/Hero"
+import { Join } from "@/components/Join/Join"
 import { Posts } from "@/components/Posts/Posts"
+import { Testimonials } from "@/components/Testimonials/Testimonials"
+import { WhyWeStarted } from "@/components/WhyWeStarted/WhyWeStarted"
 
 import styles from "./page.module.scss"
 
@@ -12,6 +18,12 @@ export default function Home({ params: { locale } }: { params: { locale: string 
     <main className={styles.main}>
       <Hero />
       <Posts />
+      <About />
+      <ChooseCategory />
+      <WhyWeStarted />
+      <Authors />
+      <Testimonials />
+      <Join />
     </main>
   )
 }
