cd93074737424b62843c0b8575c57be18455a0d6
Yan Yermalayeu <yermik2014@yandex.ru>
2024-01-02 01:05:56 +0300

    feat: add why we started locales

--- src/components/WhyWeStarted/WhyWeStarted.tsx
+++ src/components/WhyWeStarted/WhyWeStarted.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { useTranslations } from "next-intl"
 
 import whyWeStarted from "@/assets/images/why_we_started.png"
 import { Button } from "@/ui/Button/Button"
@@ -6,6 +7,7 @@ import { Button } from "@/ui/Button/Button"
 import styles from "./whyWeStated.module.scss"
 
 export function WhyWeStarted() {
+  const t = useTranslations("home.why")
   return (
     <section className={styles.why}>
       <Image
@@ -14,16 +16,16 @@ export function WhyWeStarted() {
         alt="why-we-started-background"
       />
       <div className={styles.overlay}>
-        <h4 className={styles.suptitle}>Why we started</h4>
-        <h2 className={styles.title}>
-          It started out as a simple idea and evolved into our passion
-        </h2>
+        <h4 className={styles.suptitle}>{t("suptitle")}</h4>
+        <h2 className={styles.title}>{t("title")}</h2>
         <p className={styles.text}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
           nostrud exercitation ullamco laboris nisi ut aliquip.
         </p>
-        <Button variant="primary">Discover our story {">"}</Button>
+        <Button variant="primary">
+          {t("button")} {">"}
+        </Button>
       </div>
     </section>
   )
