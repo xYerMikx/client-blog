c6e19f8f00e5ef441fedb56c1166dc7874bf3ab9
Yan Yermalayeu <yermik2014@yandex.ru>
2024-01-03 18:21:33 +0300

    fix: nextintlprovider bug

--- src/app/[locale]/posts/page.tsx
+++ src/app/[locale]/posts/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { NextIntlClientProvider, useMessages } from "next-intl"
 import { unstable_setRequestLocale } from "next-intl/server"
 
 import featuredPost from "@/assets/images/featured_post.png"
@@ -8,11 +9,13 @@ import { FeaturedBlogPost } from "@/components/FeaturedBlogPost/FeaturedBlogPost
 import { Join } from "@/components/Join/Join"
 import { blogPosts } from "@/constants/blogPosts"
 import { IParamsLocale } from "@/types/types"
+import { pickMessages } from "@/utils/pickMessages"
 
 import styles from "./blog.module.scss"
 
 export default function Blog({ params: { locale } }: IParamsLocale) {
   unstable_setRequestLocale(locale)
+  const messages = useMessages()
 
   const { title, author, createdAt, text, id } = blogPosts[0]
 
@@ -29,8 +32,10 @@ export default function Blog({ params: { locale } }: IParamsLocale) {
         <Image src={featuredPost} alt="featured-post" />
       </section>
       <BlogPosts />
-      <ChooseCategory />
-      <Join />
+      <NextIntlClientProvider messages={pickMessages(messages, "home")}>
+        <ChooseCategory />
+        <Join />
+      </NextIntlClientProvider>
     </main>
   )
 }
