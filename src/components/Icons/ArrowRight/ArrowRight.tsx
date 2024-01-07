f866150171554ed76ac25682824b1e7e57e2bc40
Yan Yermalayeu <yermik2014@yandex.ru>
2024-01-07 16:44:11 +0300

    fix: arrows

--- src/components/Icons/ArrowRight/ArrowRight.tsx
+++ src/components/Icons/ArrowRight/ArrowRight.tsx
@@ -12,7 +12,6 @@ export function ArrowRight({ onClick }: IArrowRightProps) {
       height="48"
       viewBox="0 0 48 48"
       fill="none"
-      transform="rotate(-180)"
       xmlns="http://www.w3.org/2000/svg"
     >
       <circle className={styles.circle} cx="24" cy="24" r="24" fill="white" />
