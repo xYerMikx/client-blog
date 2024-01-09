bcbc8c28c0f91fa63d1670456ec43973d298507a
Yan Yermalayeu <yermik2014@yandex.ru>
2024-01-10 02:16:41 +0300

    feat: add infinite scroll

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,8 @@
-import { NextIntlClientProvider, useMessages } from "next-intl"
+import { useMessages } from "next-intl"
 import { unstable_setRequestLocale } from "next-intl/server"
 
-import { About } from "@/components/About/About"
-import { Authors } from "@/components/Authors/Authors"
-import { ChooseCategory } from "@/components/ChooseCategory/ChooseCategory"
-import { Hero } from "@/components/Hero/Hero"
-import { Join } from "@/components/Join/Join"
-import { Posts } from "@/components/Posts/Posts"
-import { Testimonials } from "@/components/Testimonials/Testimonials"
-import { WhyWeStarted } from "@/components/WhyWeStarted/WhyWeStarted"
+import { HomeWrapper } from "@/components/HomeWrapper/HomeWrapper"
 import { IParamsLocale } from "@/types/types"
-import { pickMessages } from "@/utils/pickMessages"
 
 import styles from "./page.module.scss"
 
@@ -20,16 +12,7 @@ export default function Home({ params: { locale } }: IParamsLocale) {
 
   return (
     <main className={styles.main}>
-      <NextIntlClientProvider messages={pickMessages(messages, "home")}>
-        <Hero />
-        <Posts />
-        <About />
-        <ChooseCategory />
-        <WhyWeStarted />
-        <Authors />
-        <Testimonials />
-        <Join />
-      </NextIntlClientProvider>
+      <HomeWrapper messages={messages} locale={locale} />
     </main>
   )
 }
